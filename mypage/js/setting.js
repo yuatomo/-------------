52a87a7318574d7c5a6c18f9b4ef0d8bf039a2f5
syuncyan <sunchengtengye@gmail.com>
2023-07-03 09:38:52 +0900

    音量のメモリの追加

---
+++ mypage/js/setting.js
@@ -0,0 +1,8 @@
+  window.onload = display_value_slider01();
+  function display_value_slider01() {
+    document.getElementById('display01').innerHTML = document.getElementById('slider01').value;
+  }
+  window.onload = display_value_slider02();
+  function display_value_slider02() {
+    document.getElementById('display02').innerHTML = document.getElementById('slider02').value;
+  }
