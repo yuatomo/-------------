da9d2222a483b4c232f8fc3582c37b2128fc3879
yuatomo2 <21110023@oic-ok.ac.jp>
2023-07-27 09:49:55 +0900

    checkboxの初期値をtrueに変更

--- solveQuestion/index.php
+++ solveQuestion/index.php
@@ -42,46 +42,46 @@
                     <div class="flexgenre">
                         <div class="checkedgenre">
                             <div class="summary">
-                                <label for="check101"><input type="checkbox" id="check101" name="Genreselection" value="Genreselection">テクノロジ系</label>
+                                <label for="check101"><input type="checkbox" id="check101" name="Genreselection" value="Genreselection" checked>テクノロジ系</label>
                             </div>
                             <div class="Genreselection">
                                 <div class="checkbutton">
-                                    <label for="check1"><input type="checkbox" id="check1" name="Genreselection" value="Genreselection">基礎理論</label>
-                                    <label for="check2"><input type="checkbox" id="check2" name="Genreselection" value="Genreselection">アルゴリズムとプログラミング</label>
-                                    <label for="check3"><input type="checkbox" id="check3" name="Genreselection" value="Genreselection">システム構成要素</label>
-                                    <label for="check4"><input type="checkbox" id="check4" name="Genreselection" value="Genreselection">ソフトウェア</label>
-                                    <label for="check5"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">ハードウェア</label>
-                                    <label for="check6"><input type="checkbox" id="check6" name="Genreselection" value="Genreselection">ヒューマンインターフェース</label>
-                                    <label for="check7"><input type="checkbox" id="check7" name="Genreselection" value="Genreselection">マルチメディア</label>
-                                    <label for="check8"><input type="checkbox" id="check8" name="Genreselection" value="Genreselection">データベース</label>
-                                    <label for="check9"><input type="checkbox" id="check9" name="Genreselection" value="Genreselection">ネットワーク</label>
-                                    <label for="check10"><input type="checkbox" id="check10" name="Genreselection" value="Genreselection">セキュリティ</label>
-                                    <label for="check11"><input type="checkbox" id="check11" name="Genreselection" value="Genreselection">システム開発技術</label>
-                                    <label for="check12"><input type="checkbox" id="check12" name="Genreselection" value="Genreselection">ソフトウェア開発管理技術</label>
+                                    <label for="check1"><input type="checkbox" id="check1" name="Genreselection" value="Genreselection" checked>基礎理論</label>
+                                    <label for="check2"><input type="checkbox" id="check2" name="Genreselection" value="Genreselection" checked>アルゴリズムとプログラミング</label>
+                                    <label for="check3"><input type="checkbox" id="check3" name="Genreselection" value="Genreselection" checked>システム構成要素</label>
+                                    <label for="check4"><input type="checkbox" id="check4" name="Genreselection" value="Genreselection" checked>ソフトウェア</label>
+                                    <label for="check5"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>ハードウェア</label>
+                                    <label for="check6"><input type="checkbox" id="check6" name="Genreselection" value="Genreselection" checked>ヒューマンインターフェース</label>
+                                    <label for="check7"><input type="checkbox" id="check7" name="Genreselection" value="Genreselection" checked>マルチメディア</label>
+                                    <label for="check8"><input type="checkbox" id="check8" name="Genreselection" value="Genreselection" checked>データベース</label>
+                                    <label for="check9"><input type="checkbox" id="check9" name="Genreselection" value="Genreselection" checked>ネットワーク</label>
+                                    <label for="check10"><input type="checkbox" id="check10" name="Genreselection" value="Genreselection" checked>セキュリティ</label>
+                                    <label for="check11"><input type="checkbox" id="check11" name="Genreselection" value="Genreselection" checked>システム開発技術</label>
+                                    <label for="check12"><input type="checkbox" id="check12" name="Genreselection" value="Genreselection" checked>ソフトウェア開発管理技術</label>
                                 </div>
                                 
                             </div>
                         </div>
                         <div class="questioninput">
                             <div class="summary1">
-                                <label for="check102"><input type="checkbox" id="check102"  name="Genreselection" value="Genreselection">マネジメント系</label>
+                                <label for="check102"><input type="checkbox" id="check102"  name="Genreselection" value="Genreselection" checked>マネジメント系</label>
                             </div>
                             <div class="Genreselection1">
-                                <label for="check14"><input type="checkbox" id="check14" name="Genreselection" value="Genreselection">プロジェクトマネジメント</label>
-                                <label for="check15"><input type="checkbox" id="check15" name="Genreselection" value="Genreselection">サービスマネジメント</label>
-                                <label for="check16"><input type="checkbox" id="check16" name="Genreselection" value="Genreselection">システム監査</label>
+                                <label for="check14"><input type="checkbox" id="check14" name="Genreselection" value="Genreselection" checked>プロジェクトマネジメント</label>
+                                <label for="check15"><input type="checkbox" id="check15" name="Genreselection" value="Genreselection" checked>サービスマネジメント</label>
+                                <label for="check16"><input type="checkbox" id="check16" name="Genreselection" value="Genreselection" checked>システム監査</label>
                             </div>
                             <div class="summary1">
-                                <label for="check103"><input type="checkbox" id="check103" name="Genreselection" value="Genreselection">ストラテジ系</label>
+                                <label for="check103"><input type="checkbox" id="check103" name="Genreselection" value="Genreselection" checked>ストラテジ系</label>
                             </div>
                             <div class="Genreselection1">
-                                <label for="check17"><input type="checkbox" id="check7" name="Genreselection" value="Genreselection">システム戦略</label>
-                                <label for="check18"><input type="checkbox" id="check8" name="Genreselection" value="Genreselection">システム企画</label>
-                                <label for="check19"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">経営戦略マネジメント</label>
-                                <label for="check20"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">技術戦略マネジメント</label>
-                                <label for="check21"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">ビジネスインダストリ</label>
-                                <label for="check22"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">企業企画</label>
-                                <label for="check23"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection">法務</label>
+                                <label for="check17"><input type="checkbox" id="check7" name="Genreselection" value="Genreselection" checked>システム戦略</label>
+                                <label for="check18"><input type="checkbox" id="check8" name="Genreselection" value="Genreselection" checked>システム企画</label>
+                                <label for="check19"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>経営戦略マネジメント</label>
+                                <label for="check20"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>技術戦略マネジメント</label>
+                                <label for="check21"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>ビジネスインダストリ</label>
+                                <label for="check22"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>企業企画</label>
+                                <label for="check23"><input type="checkbox" id="check5" name="Genreselection" value="Genreselection" checked>法務</label>
                             </div>
                         </div>
                     </div>
